d0e5fc295820eabc9c675b862b91cc70e5e69cfe
frenchkebab <tubowangin@gmail.com>
2021-08-09 19:43:18 +0900

    계산기 - 중복이 있는 코드

---
+++ 4장/app.js
@@ -0,0 +1,104 @@
+let numOne = "";
+let operator = "";
+let numTwo = "";
+
+// 태그선택
+const $operator = document.querySelector("#operator");
+const $result = document.querySelector("#result");
+
+// 버튼 하나하나를 자바스크립트 동작으로 정의함
+document.querySelector("#num-0").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "0";
+  } else {
+    // 비어있다
+    numOne += "0";
+  }
+  $result.value += "0";
+});
+document.querySelector("#num-1").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "1";
+  } else {
+    // 비어있다
+    numOne += "1";
+  }
+  $result.value += "1";
+});
+document.querySelector("#num-2").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "2";
+  } else {
+    // 비어있다
+    numOne += "2";
+  }
+  $result.value += "2";
+});
+document.querySelector("#num-3").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "3";
+  } else {
+    // 비어있다
+    numOne += "3";
+  }
+  $result.value += "3";
+});
+document.querySelector("#num-4").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "4";
+  } else {
+    // 비어있다
+    numOne += "4";
+  }
+  $result.value += "4";
+});
+document.querySelector("#num-5").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "5";
+  } else {
+    // 비어있다
+    numOne += "5";
+  }
+  $result.value += "5";
+});
+document.querySelector("#num-6").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "6";
+  } else {
+    // 비어있다
+    numOne += "6";
+  }
+  $result.value += "6";
+});
+document.querySelector("#num-7").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "7";
+  } else {
+    // 비어있다
+    numOne += "7";
+  }
+  $result.value += "7";
+});
+document.querySelector("#num-8").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "8";
+  } else {
+    // 비어있다
+    numOne += "8";
+  }
+  $result.value += "8";
+});
+document.querySelector("#num-9").addEventListener("click", () => {
+  if (operator) {
+    numTwo += "9";
+  } else {
+    // 비어있다
+    numOne += "9";
+  }
+  $result.value += "9";
+});
+document.querySelector("#plus").addEventListener("click", () => {});
+document.querySelector("#divide").addEventListener("click", () => {});
+document.querySelector("#multiply").addEventListener("click", () => {});
+document.querySelector("#calculate").addEventListener("click", () => {});
+document.querySelector("#clear").addEventListener("click", () => {});
