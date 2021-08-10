0ebacc97131d431596c9852ac4a9496bd24a865e
frenchkebab <tubowangin@gmail.com>
2021-08-10 15:54:06 +0900

    연산자 작동

--- 4장/app.js
+++ 4장/app.js
@@ -8,26 +8,40 @@ const $result = document.querySelector("#result");
 
 const onClickNumber = (event) => {
   if (operator) {
+    if (!numTwo) {
+      $result.value = "";
+    }
     numTwo += event.target.textContent;
   } else {
     numOne += event.target.textContent;
+  }
+  $result.value += event.target.textContent;
+};
+
+const onClickOperator = (op) => () => {
+  if (numOne) {
+    operator = op;
+    $operator.value = op;
+  } else {
+    alert("숫자를 먼저 입력하세요.");
   }
 };
 
 // 버튼 하나하나를 자바스크립트 동작으로 정의함
 document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
-document.querySelector("#num-0").addEventListener("click", onClickNumber);
+document.querySelector("#num-1").addEventListener("click", onClickNumber);
+document.querySelector("#num-2").addEventListener("click", onClickNumber);
+document.querySelector("#num-3").addEventListener("click", onClickNumber);
+document.querySelector("#num-4").addEventListener("click", onClickNumber);
+document.querySelector("#num-5").addEventListener("click", onClickNumber);
+document.querySelector("#num-6").addEventListener("click", onClickNumber);
+document.querySelector("#num-7").addEventListener("click", onClickNumber);
+document.querySelector("#num-8").addEventListener("click", onClickNumber);
+document.querySelector("#num-9").addEventListener("click", onClickNumber);
 
-document.querySelector("#plus").addEventListener("click", () => {});
-document.querySelector("#divide").addEventListener("click", () => {});
-document.querySelector("#multiply").addEventListener("click", () => {});
+document.querySelector("#plus").addEventListener("click", onClickOperator("+"));
+document.querySelector("#minus").addEventListener("click", onClickOperator("-"));
+document.querySelector("#divide").addEventListener("click", onClickOperator("*"));
+document.querySelector("#multiply").addEventListener("click", onClickOperator("/"));
 document.querySelector("#calculate").addEventListener("click", () => {});
 document.querySelector("#clear").addEventListener("click", () => {});
