7dca7b56ba55157a63f3fc8a2044596cc47e02f5
frenchkebab <tubowangin@gmail.com>
2021-08-12 14:31:14 +0900

    숫자야구 구현

--- 4장/app.js
+++ 4장/app.js
@@ -6,27 +6,27 @@ const $operator = document.querySelector("#operator");
 const $result = document.querySelector("#result");
 
 const onClickNumber = (event) => {
-  if (!operator) {
-    // 비어있다
-    numOne += event.target.textContent;
+    if (!operator) {
+        // 비어있다
+        numOne += event.target.textContent;
+        $result.value += event.target.textContent;
+        return;
+    }
+    // 비어있지 않다
+    if (!numTwo) {
+        $result.value = "";
+    }
+    numTwo += event.target.textContent;
     $result.value += event.target.textContent;
-    return;
-  }
-  // 비어있지 않다
-  if (!numTwo) {
-    $result.value = "";
-  }
-  numTwo += event.target.textContent;
-  $result.value += event.target.textContent;
 };
 
 const onClickOperator = (op) => () => {
-  if (numOne) {
-    operator = op;
-    $operator.value = op;
-  } else {
-    alert("숫자를 먼저 입력하세요.");
-  }
+    if (numOne) {
+        operator = op;
+        $operator.value = op;
+    } else {
+        alert("숫자를 먼저 입력하세요.");
+    }
 };
 
 // 버튼 하나하나를 자바스크립트 동작으로 정의함
@@ -42,37 +42,43 @@ document.querySelector("#num-8").addEventListener("click", onClickNumber);
 document.querySelector("#num-9").addEventListener("click", onClickNumber);
 
 document.querySelector("#plus").addEventListener("click", onClickOperator("+"));
-document.querySelector("#minus").addEventListener("click", onClickOperator("-"));
-document.querySelector("#divide").addEventListener("click", onClickOperator("/"));
-document.querySelector("#multiply").addEventListener("click", onClickOperator("*"));
+document
+    .querySelector("#minus")
+    .addEventListener("click", onClickOperator("-"));
+document
+    .querySelector("#divide")
+    .addEventListener("click", onClickOperator("/"));
+document
+    .querySelector("#multiply")
+    .addEventListener("click", onClickOperator("*"));
 document.querySelector("#calculate").addEventListener("click", () => {
-  if (numTwo) {
-    switch (operator) {
-      case "+":
-        $result.value = parseInt(numOne) + parseInt(numTwo);
-        break;
-      // 빼기, 곱하기, 나누기는 문자열이 숫자로 바뀐다!
-      case "-":
-        $result.value = numOne - numTwo;
-        break;
-      case "*":
-        $result.value = numOne * numTwo;
-        break;
-      case "/":
-        $result.value = numOne / numTwo;
-        break;
-      default:
-        break;
+    if (numTwo) {
+        switch (operator) {
+            case "+":
+                $result.value = parseInt(numOne) + parseInt(numTwo);
+                break;
+            // 빼기, 곱하기, 나누기는 문자열이 숫자로 바뀐다!
+            case "-":
+                $result.value = numOne - numTwo;
+                break;
+            case "*":
+                $result.value = numOne * numTwo;
+                break;
+            case "/":
+                $result.value = numOne / numTwo;
+                break;
+            default:
+                break;
+        }
+    } else {
+        alert("숫자를 먼저 입력하세요.");
     }
-  } else {
-    alert("숫자를 먼저 입력하세요.");
-  }
 });
 
 document.querySelector("#clear").addEventListener("click", () => {
-  numOne = "";
-  operator = "";
-  numTwo = "";
-  $operator.value = "";
-  $result.value = "";
+    numOne = "";
+    operator = "";
+    numTwo = "";
+    $operator.value = "";
+    $result.value = "";
 });
---
+++ 5장/셀프체크/app.js
@@ -0,0 +1,94 @@
+const $input = document.querySelector('#input');
+const $form = document.querySelector('#form');
+const $logs = document.querySelector('#logs');
+
+const numbers = [];
+for (let n = 1; n < 10; n++) {
+    numbers.push(n);
+}
+
+const answer = [];
+for (let n = 0; n < 4; n += 1) {
+    const index = Math.floor(Math.random() * numbers.length);
+    answer.push(numbers[index]);
+    numbers.splice(index, 1);
+}
+
+console.log(answer);
+
+const tries = [];
+
+function checkInput(input) {
+    // 검사하는 코드
+    if (input.length !== 4) {
+        return alert('4자리 숫자를 입력해 주세요.');
+    }
+    // Set을 만들어줌 (중복을 제거한 배열)
+    if (new Set(input).size !== 4) {
+        return alert('중복되지 않게 입력해 주세요.');
+    }
+    // 이미 시도한 값이 아닌지
+    if (tries.includes(input)) {
+        return alert('이미 시도한 값입니다.');
+    }
+    return true;
+}
+
+let out = 0;
+function defeated() {
+    $logs.append(`패배! 정답은 ${answer.join('')}`);
+    return;
+}
+
+$form.addEventListener('submit', (event) => {
+    event.preventDefault(); // 기본 동작 막기
+    const value = $input.value;
+    // const value = event.target.value 라고 해도 됨!
+    $input.value = '';
+
+    // 통과하지 않은 애들은 버려서 펴주는 테크닉
+    // if(checkInput(value)){
+    // }else{
+    // }
+    const valid = checkInput(value);
+    if (!valid) return;
+
+    // 입력값 문제 없음
+    if (answer.join('') === value) {
+        // [3, 1, 4, 6] -> '3146'
+        $logs.textContent = '홈런!';
+        return;
+    }
+    if (tries.length >= 9) {
+        defeated();
+        return;
+    }
+
+    // 몇 스트라이크 몇 볼인지 검사
+    let strike = 0;
+    let ball = 0;
+
+    // answer: 3146, value: 1234
+    for (let i = 0; i < answer.length; i++) {
+        const index = value.indexOf(answer[i]);
+        // 일치하는 숫자 발견
+        if (index > -1) {
+            // index까지 같으면
+            if (index === i) {
+                strike += 1;
+            } else {
+                ball += 1;
+            }
+        }
+    }
+    $logs.append(`${value}: ${strike} 스트라이크 ${ball} 볼`, document.createElement('br'));
+    if (strike == 0 && ball == 0) {
+        out += 1;
+        $logs.append(`${out}아웃`, document.createElement('br'));
+        if (out >= 3) {
+            defeated();
+            return;
+        }
+    }
+    tries.push(value);
+});
